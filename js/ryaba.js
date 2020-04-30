bef0dcf9ff63346c48a63e8da4ea7d3ec7a3a1e1
KseniaPol <63474241+KseniaPol@users.noreply.github.com>
2020-04-30 17:08:33 +0400

    Add files via upload

---
+++ js/ryaba.js
@@ -0,0 +1,48 @@
+const dataURL = "https://api.jsonbin.io/b/5e905926172eb643896166e7";
+const fields = [
+	"var1",
+	"var2",
+	"var3",
+	"var4",
+	"var5",
+	"var6",
+	"speach"
+]
+
+function getFormValues() {
+	let obj = {};
+
+	fields.forEach(function(field) {
+		obj[field] = $("input[name=" + field + "]")[0].value
+	});
+
+	return obj;
+}
+
+function handleButton(event) {
+	$.getJSON(dataURL, handleData);
+
+	event.preventDefault();
+}
+
+function handleData(data) {
+	let message = "";
+
+	let values = getFormValues();
+
+	data["text"].forEach(function(line) {
+		for (key in values) {
+			line = line.replace("{" + key + "}", values[key]);
+		}
+
+		message = message + line + "<br>";
+	});
+
+    $("div#result").html(message);
+}
+
+function init() {
+	$("#button-fetch").click(handleButton);
+}
+
+$(document).ready(init);
